3703cd84914a66fdf396a168b6e0dd686c1c58a3
NgoVanHuy2001 <huyhhbgst@gmail.com>
2021-11-25 15:58:23 +0700

    pipe

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,11 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  public traiCay = ["Táo", "Nho", "Cam", "Quýt", "Mít"];
+  public traiCay = ['Táo', 'Nho', 'Cam', 'Quýt', 'Mít'];
+  public traiCay2 = [
+    {ten: 'Táo', gia: 15, haGia: true},
+    {ten: 'Nho', gia: 2, haGia: false},
+    {ten: 'Cam', gia: 3, haGia: false},
+    {ten: 'Quýt', gia: 30, haGia: true},
+    {ten: 'Mít', gia: 40, haGia: true}
+];
   constructor() { }
 
   ngOnInit(): void {
-    console.log('trai cay = ', this.traiCay)
+    console.log('trai cay = ', this.traiCay2)
   }
 
 }
