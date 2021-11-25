74bd696bfc4d0c2c79ca68c4a22290809ce5f336
NgoVanHuy2001 <huyhhbgst@gmail.com>
2021-11-25 15:15:36 +0700

    ngFor

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public traiCay = ["Táo", "Nho", "Cam", "Quýt", "Mít"];
   constructor() { }
 
   ngOnInit(): void {
+    console.log('trai cay = ', this.traiCay)
   }
 
 }
